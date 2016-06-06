db9a0eb73c9f13a4cb4553061ba45481164385f2
Khang Nguyen-Le <khangnlh@dgroup.co>
2016-06-06 10:06:51 +0700

    Add slides and readme file

---
+++ README.md
@@ -0,0 +1,13 @@
+# Meteor React Boilerplate
+
+To init the app please run:
+
+    npm install
+
+To start server please run:
+
+    meteor
+
+The app will be served at [localhost:3000](http://localhost:3000)
+
+The slides will be served at [Meteor - Structuring React Container](http://localhost:3000/slides/slides.html)
--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -6,8 +6,15 @@ import { Layout } from '../../ui/layouts/Layout.jsx';
 
 import CounterContainer from '../../ui/pages/CounterContainer/CounterContainer.js';
 
-FR.route('/:multiplier', {
+FR.route('/', {
   name: 'Home',
+  triggersEnter: [function redirectFunc(context, redirect) {
+    redirect('/1');
+  }],
+});
+
+FR.route('/:multiplier', {
+  name: 'Counter',
   action(params) {
     mount(Layout, {
       content: <CounterContainer {...params} />,
